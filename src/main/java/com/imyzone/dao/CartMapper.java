950c9962b5234f1ffc0cd33b2de333f5ed2df43f
fangchenhui <546153136@qq.com>
2017-10-06 16:47:55 +0800

    项目初始化

---
+++ src/main/java/com/imyzone/dao/CartMapper.java
@@ -0,0 +1,19 @@
+package com.imyzone.dao;
+
+import com.imyzone.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+
+
+}
---
+++ src/main/java/com/imyzone/dao/ShippingMapper.java
@@ -0,0 +1,17 @@
+package com.imyzone.dao;
+
+import com.imyzone.pojo.Shipping;
+
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
